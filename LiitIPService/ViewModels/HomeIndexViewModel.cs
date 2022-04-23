78ac5d2df832495089c00f3d9168346f15f7fbe2
Linn Johansson <epostlinn@outlook.com>
2022-04-23 22:33:16 +0200

    EmployeeViewModel

--- LiitIPService/ViewModels/HomeIndexViewModel.cs
+++ LiitIPService/ViewModels/HomeIndexViewModel.cs
@@ -8,5 +8,12 @@ namespace LiitIPService.ViewModels
     public class HomeIndexViewModel
     {
         //mer kod
+        public class Employee
+        {
+            public int EmployeeId { get; set; }
+            public string EmployeeFirstName { get; set; }
+            public string EmployeeLastName { get; set; }
+        }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
